a6cf7f0193f8473c44051aead20058489d6d775f
Ahmed <85339083+PhoneWyne@users.noreply.github.com>
2024-08-07 21:01:30 +0500

    First commit, into shoes e-commerce project

---
+++ src/data/nfts.js
@@ -0,0 +1,32 @@
+import card2 from '../assets/card2.png';
+import card3 from '../assets/card3.png';
+import card4 from '../assets/card4.png';
+
+export const nfts = [
+  {
+    id: 1,
+    title: 'Nike Dunk Low Retro',
+    reference: 'Men\'s Shoes',
+    author: 'Nike',
+    
+    priceUsd: 115,
+    img: card2,
+  },
+  {
+    id: 1,
+    title: 'Nike Pegasus Plus',
+    reference: 'Men\'s Shoes',
+    author: 'Nike',
+    
+    priceUsd: 180,
+    img: card3,
+  },
+  {
+    id: 1,
+    title: 'Nike Air Max Dn',
+    reference: 'Men\'s Shoes',
+    author: 'Nike',
+    priceUsd: 125,
+    img: card4,
+  }
+];
